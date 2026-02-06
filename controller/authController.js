faab6a8d0ed69231572dfd074293b0ce3d021ae4
Sanjana <sanjanasapar@gmail.com>
2026-02-06 13:28:45 +0000

    Fixed the bug in auth controller

--- controller/authController.js
+++ controller/authController.js
@@ -2,7 +2,6 @@ const User = require('../models/UserModel');
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler');
 
-
 const maxAge = 2 * 24 * 60 * 60;
 
 const createToken = (id) => {
@@ -11,38 +10,52 @@ const createToken = (id) => {
     });
 };
 
-const userSignup = asyncHandler(async (req, res, next) => {
-
+const userSignup = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
+
+
     const user = await User.create({ email, password });
+
     const token = createToken(user._id);
-    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        maxAge: maxAge * 1000,
+
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax'
+    });
+
     res.status(201).json({ user: user._id });
-    next();
 
-
-})
-
-const userLogin = asyncHandler(async (req, res, next) => {
-
-    const { email, password } = req.body;
-    const user = await User.login(email, password);
-    const token = createToken(user._id);
-    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-    res.status(200).json({ user: user._id });
-    next();
 });
 
-const userLogout = (req, res,) => {
+const userLogin = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+
+    const user = await User.login(email, password);
+
+    const token = createToken(user._id);
+
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        maxAge: maxAge * 1000,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax'
+    });
+
+    res.status(200).json({ user: user._id });
+
+});
+
+const userLogout = (req, res) => {
     res.cookie('jwt', '', {
         httpOnly: true,
         maxAge: 1
     });
     res.status(200).json({ message: "Logged out successfully" });
 }
-
-
-
 
 module.exports = {
     userSignup,
